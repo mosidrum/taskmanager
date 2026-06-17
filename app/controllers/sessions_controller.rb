31f16aaf879bed820357e4c81cca1753495a26f9
mosidrum <mosiokanga@gmail.com>
2026-06-17 20:43:25 +0100

    feat: add login page, sessions controller, and signup redesign

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  layout "auth"
+
+  def new; end
+
+  def create
+    user = authenticate_user
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = I18n.t("sessions.invalid_credentials")
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+  private
+
+  def authenticate_user
+    user = User.find_by(email: params[:email])
+    user&.authenticate(params[:password])
+  end
+end
